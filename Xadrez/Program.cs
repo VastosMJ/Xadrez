a38dad7e5a63ab058bb88891c00714c163fb22f3
Marcos Junior <juninhooliveira.22@hotmail.com>
2019-02-24 10:15:27 -0300

    Primeira classe tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Xadrez.Tabuleiro;
 
 namespace Xadrez
 {
@@ -6,7 +7,9 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            
         }
     }
 }
